5eaaf34ad7cf8a75eb75e4dbf5c4bf78f4b3e424
swapnilg671 <swapnilg671@gmail.com>
2021-08-29 11:50:47 +0530

    creating classes and objects

---
+++ p1/javaAccessModifier.java
@@ -0,0 +1,13 @@
+package p1;
+import p1.a;
+
+public class javaAccessModifier {
+    public static void main(String[] args){
+        A aObject= new A();
+        System.out.println(aObject.d);
+        C cObject= new C();
+        System.out.println(cObject.c);
+        System.out.println(cObject.d);        
+
+    }
+}
