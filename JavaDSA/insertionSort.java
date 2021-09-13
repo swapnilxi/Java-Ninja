3b9f5dd2beb85df1f35ffd6a101004d989cecedf
swapnilg671 <swapnilg671@gmail.com>
2021-09-13 10:21:28 +0530

    thread methods

---
+++ JavaDSA/insertionSort.java
@@ -0,0 +1,16 @@
+// time complexity- O(N^2)
+// stable algorithm 
+
+package JavaDSA;
+public class insertionSort {
+public static void main(String[] args) {
+    int [] intArray={20,35,-15,7,55,1, -22};
+// zero index position is in sorted position 
+// the unsorted portion start from 1 and we travase and insert no. at their palce
+    for( int firstUnsortedIndex=1; firstUnsortedIndex< intArray.length; firstUnsortedIndex++){
+        
+    }
+
+
+}    
+}
