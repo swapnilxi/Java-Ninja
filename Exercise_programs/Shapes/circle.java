0cde52bad05a141231750c71a4bb8f6d654773d8
swapnilg671 <swapnilg671@gmail.com>
2021-09-13 16:30:11 +0530

    insertion sort

---
+++ Exercise_programs/Shapes/circle.java
@@ -0,0 +1,10 @@
+// package Exercise_programs.Shapes.ShapeClass.circle;
+
+// public class circle extends Shapes {
+//     circle(int dim1){
+//         super(dim1, -1);
+//     }
+//     public double area(){
+//        return Math.PI * dim1 * dim1;
+//     }
+// }
--- JavaDSA/insertionSort.java
+++ JavaDSA/insertionSort.java
@@ -1,16 +1,26 @@
-// time complexity- O(N^2)
-// stable algorithm 
-
 package JavaDSA;
+//time complexity- O(N^2)
+//stable algorithm 
 public class insertionSort {
 public static void main(String[] args) {
     int [] intArray={20,35,-15,7,55,1, -22};
 // zero index position is in sorted position 
 // the unsorted portion start from 1 and we travase and insert no. at their palce
     for( int firstUnsortedIndex=1; firstUnsortedIndex< intArray.length; firstUnsortedIndex++){
+        int newElement=intArray[firstUnsortedIndex];
+        int i;  
         
+        for(i= firstUnsortedIndex; i>0 && intArray[i-1]> newElement; i-- ){
+            intArray[i]= intArray[i-1];
+        }
+
+        intArray[i]= newElement;
     }
 
 
-}    
+
+ for (int i; i< intArray.length; i++){
+     System.out.println(intArray[i]);
+ }
+}   
 }
