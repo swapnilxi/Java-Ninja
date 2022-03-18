e13711b396d167dcf112a7bafb4793c33012b526
swapnil-hp <swapnilg671@gmail.com>
2022-03-18 20:02:35 +0530

    Dijiksta's algorithm

---
+++ JavaDSA/JavaDSA1/queuell.java
@@ -0,0 +1,18 @@
+package JavaDSA1;
+class Node{
+    int data;
+    Node next;
+    public Node(int data){
+        this.data= data;
+    }
+}
+
+public class queuell {
+    public static void main(String[] args) {
+    
+        
+        Node front, rear;
+        
+    }
+    
+}
