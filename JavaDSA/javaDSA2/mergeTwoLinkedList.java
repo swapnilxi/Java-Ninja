a45a50db952b323a6ccdf12600e0aa791b72dc58
Gupta <swapnilg671@gmail.com>
2022-04-15 22:36:00 +0530

    maximum depth

--- JavaDSA/javaDSA2/mergeTwoLinkedList.java
+++ JavaDSA/javaDSA2/mergeTwoLinkedList.java
@@ -1,26 +1,44 @@
-if(l1 == null) return l2;
-        if(l2 == null) return l1;
-        ListNode tmp,ans;
-        if(l1.val <= l2.val){
-            tmp = l1;
-            l1 = l1.next;
-        }else{
-            tmp = l2;
-            l2 = l2.next;
-        }
-        ans = tmp;
-
-        while(l1 != null && l2 != null){
-            if(l1.val <= l2.val){
-                tmp.next = l1;
-                tmp = tmp.next;
-                l1 = l1.next;
-            }else{
-                tmp.next = l2;
-                tmp = tmp.next;
-                l2 = l2.next;
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        
+        //Define a new Node
+        ListNode answer = new ListNode();
+        ListNode newNode = answer;
+        
+        //If list1 has no element then add list2 to new node
+        if (list1 == null)
+           return newNode.next = list2;
+        
+        //If list2 has no element then add list1 to new node
+        if (list2 == null)
+            return newNode.next = list1;
+        
+        
+        //Use Merge sort logic to merge nodes value
+        while (list1 != null && list2 != null)
+        {
+            if (list1.val <= list2.val)
+            {
+                newNode.next = list1;
+                list1 = list1.next;
             }
+            else 
+            {
+                newNode.next = list2;
+                list2 = list2.next;
+            }
+            newNode = newNode.next;
         }
-        if(l2 == null) tmp.next = l1;
-        else tmp.next = l2;
-        return ans;
+    
+        //If any node left in list1
+        if (list1 != null)
+            newNode.next = list1;
+     
+        //If any node left in list2
+        if (list2 != null)
+            newNode.next = list2;
+        
+        return answer.next;
+
+    }
+}
