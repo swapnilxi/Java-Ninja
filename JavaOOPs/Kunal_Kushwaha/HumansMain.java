02d483bd1b889dca689781399385f373bacde742
Swapnil Gupta <54740310+swapnilxi@users.noreply.github.com>
2023-02-15 20:08:18 +0000

    new commit

---
+++ JavaOOPs/Kunal_Kushwaha/HumansMain.java
@@ -0,0 +1,11 @@
+package JavaOOPs.Kunal_Kushwaha;
+
+public class HumansMain {
+    public static void main(String[] args) {
+        Human swapnil =new Human(21, "swapnil", 90000, false);
+        Human kunal =new Human(50, "kunal", 80000, true);
+        System.out.println("swapnil.population: " + swapnil.population );
+        System.out.println("kunal.population: " + kunal.population );
+    }
+    
+}
