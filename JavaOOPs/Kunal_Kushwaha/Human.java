c7a828626269ff4aae7ff4fb3076505a4bb4f720
swapnil gupta <swapnilg671@gmail.com>
2023-02-16 14:13:30 +0530

    adding static method

--- JavaOOPs/Kunal_Kushwaha/Human.java
+++ JavaOOPs/Kunal_Kushwaha/Human.java
@@ -5,13 +5,25 @@ public class Human {
     String name;
     int salary;
     boolean married;
+
    static long population;
+   static void message(){
+    System.out.println("this is message to all humans");
+    //cannot do
+    //System.out.println(this.age);
+   }
+
+
+
+
     public Human(int age, String name, int salary, boolean married ) {
         this.age = age;
         this.name = name;
         this.salary = salary;
         this.married = married;
-        population += 1;
+        //this.population +=1 //not conventional
+        Human.population += 1;
+        Human.message();
     }
 
 }
